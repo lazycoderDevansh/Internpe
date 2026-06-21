fdc13f3a363c8677e2f3bbf3f6ddd37a337d3184
lazycoderDevansh <heydevansh19@gmail.com>
2026-06-21 21:47:03 +0530

    Add files via upload

---
+++ week1_number_guessing_game.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+
+    srand((unsigned int)time(NULL));
+    int number= (rand()%100)+1;
+
+    int guess=0;
+    do{
+        cout<<"Enter Guess(1-100):";
+        cin>> guess;
+        if(guess>number) cout<<"Guess Lower!"<<endl;
+        else if(guess<number) cout<<"Guess higher!"<<endl;
+        else cout<<"You won!"<<endl;
+    }while(guess!= number);
+return 0;}
